80bf14862af87c9ea5da843eaf019c24868e2766
abrilugaldevelasco <abrilugaldevelasc@gmail.com>
2026-03-08 14:30:48 -0700

    Implement node creation and value assignment

--- nodo/main.c
+++ nodo/main.c
@@ -4,27 +4,14 @@
 int main()
 {
     printf("Prueba del TAD Nodo\n");
-
-    /* TODO
-       1 Crear un nodo con valor 10
-    */
-    Nodo* n = NULL;
-
-    /* TODO
-       2 Mostrar el valor del nodo usando obtenerValor
-    */
-
-    /* TODO
-       3 Cambiar el valor a 25 usando asignarValor
-    */
-
-    /* TODO
-       4 Mostrar nuevamente el valor
-    */
-
-    /* TODO
-       5 Liberar el nodo con destruirNodo
-    */
+    Nodo* n = crearNodo(10);
+    if (n != NULL) {
+        printf("Valor actual del nodo: %d\n", obtenerValor(n));
+    asignarValor(n, 25);
+    if (n != NULL) {
+        printf("Nuevo valor tras asignarValor: %d\n", obtenerValor(n));
+    destruirNodo(n);
+    printf("Memoria liberada correctamente.\n");
 
     return 0;
 }
